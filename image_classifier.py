01d5b0fd477e32178807e153e2205321ca629a7b
VickoT <vicko.tornblom@gmail.com>
2024-05-06 12:57:14 +0200

    Add argparser to main script

--- image_classifier.py
+++ image_classifier.py
@@ -1,12 +1,17 @@
 import pandas as pd
+import argparse
 from image_separator import image_separator
 
 
 
 def main():
-    separat:
+    parser = argparse.ArgumentParser(description="Separate images based on directory path.")
+    parser.add_argument('-raw_dir', '--raw_directory', type=str, required=True,
+                        help='The path to the directoyr containing the raw data diles (csv, tif)')
+    args = parser.parse_args()
+    raw_directory = args.raw_directory
 
-
+    image_separator(raw_directory)
 
 if __name__ == "__main__":
     main()
